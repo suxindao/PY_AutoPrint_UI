5755b117a66e88bd1514d2d65762c42b1e449f04
DESKTOP-BEFC36O\suxin <suxindao@126.com>
2025-06-01 22:24:04 +0800

    设置字体大小

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFont
 from ui.main_window import MainWindow
 
 
 def main():
     app = QApplication(sys.argv)
+
+    # 全局字体设置
+    font = QFont()
+    font.setFamily("Microsoft YaHei")
+    font.setPointSize(14)  # 基础大小
+    # font.setBold(True)  # 标题加粗
+    app.setFont(font)
 
     # 设置应用程序样式
     app.setStyle('Fusion')
